fab629eb2b21f5eae68edac72fe44442c87feb10
artyomchudakov <artchudakov@gmail.com>
2022-09-11 22:46:47 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [quote, setQuote] = useState({
+    text: 'Do more than dream: work.',
+    author: 'William Arthur Ward',
+  });
+  async function fetchRandomQoute() {
+    try {
+      const quotes = await getAllQuotes('https://type.fit/api/quotes');
+      const length = quotes.length;
+      const randomNumber = Math.floor(Math.random() * length);
+      const randomQuote = quotes[randomNumber];
+      setQuote({
+        text: randomQuote.text,
+        author: randomQuote.author || 'Author is unknown',
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchRandomQoute();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="quote-box">
+      <div id="text">"{quote.text}"</div>
+      <div id="author">{quote.author}</div>
+      <div className="btn-container">
+        <div className="btn">
+          <button id="new-quote" onClick={fetchRandomQoute}>
+            New Quote
+          </button>
+        </div>
+        <div className="btn">
+          <a
+            href="https://twitter.com/intent/tweet"
+            id="tweet-quote"
+            target="_blank"
+            rel="noreferrer"
+          >
+            tweet
+          </a>
+        </div>
+      </div>
     </div>
   );
+}
+
+async function getAllQuotes(url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    return console.log(err);
+  }
 }
 
 export default App;
