2697699b69b069a3851412a0be1f6cb978b5f82c
artyomchudakov <artchudakov@gmail.com>
2022-09-12 12:13:02 +0200

    axios, code refactoring, styling error and loading messages

--- src/RandomQuote.js
+++ src/RandomQuote.js
@@ -1,63 +1,75 @@
 import './RandomQuote.css';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function RandomQuote() {
-  const [quote, setQuote] = useState({
-    text: 'Do more than dream: work.',
-    author: 'William Arthur Ward',
-  });
+  const [quote, setQuote] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   async function fetchRandomQoute() {
     try {
-      const quotes = await getAllQuotes('https://type.fit/api/quotes');
+      const response = await axios.get('https://type.fit/api/quotes');
+
+      const quotes = response.data;
       const length = quotes.length;
+
       const randomNumber = Math.floor(Math.random() * length);
       const randomQuote = quotes[randomNumber];
-      setQuote({
-        text: randomQuote.text,
-        author: randomQuote.author || 'Author is unknown',
-      });
+
+      setQuote(randomQuote);
+      setError(null);
     } catch (err) {
-      console.log(err);
+      setError(err.message);
+      setQuote(null);
+    } finally {
+      setLoading(false);
     }
   }
-
   useEffect(() => {
     fetchRandomQoute();
   }, []);
 
   return (
-    <div id="quote-box">
-      <div id="text">"{quote.text}"</div>
-      <div id="author">{quote.author}</div>
-      <div className="btn-container">
-        <div className="btn">
-          <button id="new-quote" onClick={fetchRandomQoute}>
-            New Quote
-          </button>
-        </div>
-        <div className="btn">
-          <a
-            href="https://twitter.com/intent/tweet"
-            id="tweet-quote"
-            target="_blank"
-            rel="noreferrer"
-          >
-            tweet
-          </a>
-        </div>
+    <section>
+      <div>
+        {loading && <div className="loading align-center"> Please wait a moment ...</div>}
+        {error && (
+          <div className="error align-center">{`There is a problem fetching the data - ${error}`}</div>
+        )}
+        {quote && (
+          <div id="quote-box">
+            <div id="text">"{quote.text}"</div>
+            <div id="author">({quote.author})</div>
+            <div className="btn-container">
+              <div className="btn">
+                <button id="new-quote" onClick={fetchRandomQoute}>
+                  show other Quote
+                </button>
+              </div>
+              <div className="btn">
+                <a
+                  href="https://twitter.com/intent/tweet"
+                  id="tweet-quote"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  tweet
+                </a>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
-  );
-}
 
-async function getAllQuotes(url) {
-  try {
-    const res = await fetch(url);
-    const data = await res.json();
-    return data;
-  } catch (err) {
-    return console.log(err);
-  }
+      <div class="description">
+        <p>
+          freeCodeCamp - Front End Development Libraries Projects - Build a Random Quote
+          Machine
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default RandomQuote;
